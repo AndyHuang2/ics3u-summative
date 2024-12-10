b7e29b7a9714771b900bed35bce62507ffca000c
Andy Huang <andy.huang7@student.tdsb.on.ca>
2024-12-10 13:54:40 -0500

    Added firebase to the project

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBPQiAQ4evMnd24bB37qJor8yyqi3jhczQ",
+    authDomain: "summative-a8def.firebaseapp.com",
+    projectId: "summative-a8def",
+    storageBucket: "summative-a8def.firebasestorage.app",
+    messagingSenderId: "598891006310",
+    appId: "1:598891006310:web:ec478d180b716ad2f060a7"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
